b05fe108263c680749b7c70713fd131718a412a3
SHMERNIE <fredony.ernesto@gmail.com>
2024-09-02 14:29:56 -0700

    was able to write routes for the server to respond to the client when the user write and saves a note

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,12 +1,25 @@
 const notes = require('express').Router();
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+notes.get('/notes', (req, res) =>{
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 notes.post('/', (req, res) => {
     console.log(req.body);
 
     const { title, text } = req.body;
 
-   if(req.body){
+    if(req.body){
+        readAndAppend(req.body, './db/db.json')
+        res.json(`Note added successfully`);
+    } else {
+        res.errored('Error in adding note')
+    }
+});
 
-   }
-   res.json()
-})
+module.exports = notes;
